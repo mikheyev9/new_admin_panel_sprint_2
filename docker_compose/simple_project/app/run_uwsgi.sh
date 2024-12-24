180651629e4e4d49178f308c46b4a13c76fc1481
mikheyev9 <mikheyev9@gmail.com>
2024-12-25 02:13:33 +0300

    uwsgi config

--- docker_compose/simple_project/app/run_uwsgi.sh
+++ docker_compose/simple_project/app/run_uwsgi.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
+# Устанавливаем текущую директорию как директорию скрипта
+cd "$(dirname "$0")"
 
-chown www-data:www-data /var/log
+mkdir -p /var/log/uwsgi
+chown -R www-data:www-data /var/log/uwsgi
+chown -R www-data:www-data ./staticfiles
+chown -R www-data:www-data ./media
 
-uwsgi --strict --ini /etc/app/uwsgi.ini
+uwsgi --strict --ini ./uwsgi.ini
