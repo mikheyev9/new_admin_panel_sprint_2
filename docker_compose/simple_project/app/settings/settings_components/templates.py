5210b6a4dfcd7a710935afbd3dcafaea27aa416a
mikheyev9 <mikheyev9@gmail.com>
2025-01-06 21:53:25 +0300

    feat: moved HTML template to templates directory and configured shared TEMPLATES for all apps

--- docker_compose/simple_project/app/settings/settings_components/templates.py
+++ docker_compose/simple_project/app/settings/settings_components/templates.py
@@ -1,7 +1,9 @@
+from django.conf import settings
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [settings.BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
