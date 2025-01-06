c010b41e8e17d9eced17678987af146b142e8050
mikheyev9 <mikheyev9@gmail.com>
2025-01-06 22:50:19 +0300

    refactor: use middleware instead of signals for logging IP addr

--- docker_compose/simple_project/app/settings/apps.py
+++ docker_compose/simple_project/app/settings/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class ExampleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'settings'
-
-    def ready(self) -> None:
-        from settings import signals  # noqa: F401
--- docker_compose/simple_project/app/settings/settings_components/middleware.py
+++ docker_compose/simple_project/app/settings/settings_components/middleware.py
@@ -9,6 +9,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'settings.middleware.LogClientIPMiddleware'
 ]
 
 if settings.DEBUG:
